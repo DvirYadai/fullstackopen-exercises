8591a286a0340e1be29b3bdfa8bce7a04fe951dd
DvirYadai <74467515+DvirYadai@users.noreply.github.com>
2021-03-07 14:49:19 +0200

    Answer for 3.8 q

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 
+morgan.token("type", (req) => {
+  return JSON.stringify(req.body);
+});
 app.use(express.json());
-app.use(morgan("tiny"));
+app.use(
+  morgan(":method :url :status :response-time ms - :res[content-length] :type")
+);
 
 let phoneBook = [
   {
